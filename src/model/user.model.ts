9c26f3e8b2bfaa8891fd40f4ad4216ea7acd93e8
pross1312 <dangvinhtuong12@gmail.com>
2024-12-01 16:14:50 +0700

    fix potential bugs in user.model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -22,8 +22,7 @@ class User {
 const UserModel = {
     save: async (user: User) => {
         const fields: Array<string> = Object.getOwnPropertyNames(user)
-                                            .filter(prop => user[prop as keyof User] !== undefined &&
-                                                            user[prop as keyof User] !== null &&
+                                            .filter(prop => user[prop as keyof User] !== null &&
                                                             typeof user[prop as keyof User] !== "function");
         const args: Array<string> = Array.from(fields, (field) => user[field as keyof User]!.toString());
         const params: string = Array.from({length: fields.length}, (_, index: number) => index).map(i => `$${i+1}`).join(", ");
@@ -31,16 +30,17 @@ const UserModel = {
         debugLog(query, params, args);
         await repo.exec("none", query, args);
     },
+
     find: async (info: {
         email?: string,
         password?: string,
         name?: string,
         avatar?: string
     }): Promise<Array<User>> => {
-        const fields: Array<string> = Object.getOwnPropertyNames(info).filter(val => val !== undefined);
+        const fields: Array<string> = Object.getOwnPropertyNames(info)
+                                            .filter(prop => info[prop as keyof typeof info] !== undefined);
         const condition: string = fields.map((field, index) => `${field} = $${index+1}`).join(", ");
-        const args: Array<string> = Object.values(info)
-                                          .map(val => val.toString());
+        const args: Array<string> = Array.from(fields, (field) => info[field as keyof typeof info]!.toString());
         let query: string;
         if (fields.length > 0) {
             query = `SELECT * FROM "users" WHERE ${condition}`;
@@ -51,16 +51,18 @@ const UserModel = {
         const users: Array<User> = await repo.exec("many", query, args);
         return users;
     },
+
     findOne: async (info: {
         email?: string,
         password?: string,
         name?: string,
         avatar?: string
     }): Promise<User | null> => {
-        const fields: Array<string> = Object.getOwnPropertyNames(info).filter(val => val !== undefined);
+        const fields: Array<string> = Object.getOwnPropertyNames(info)
+                                            .filter(prop => info[prop as keyof typeof info] !== undefined);
         const condition: string = fields.map((field, index) => `${field} = $${index+1}`).join(", ");
-        const args: Array<string> = Object.values(info)
-                                          .map(val => val.toString());
+        const args: Array<string> = Array.from(fields, (field) => info[field as keyof typeof info]!.toString());
+
         let query: string;
         if (fields.length > 0) {
             query = `SELECT * FROM "users" WHERE ${condition}`;
