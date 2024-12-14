6a7c4aaa7a8cb7979f4184858939458c5583a5b2
TrinhLongVu <vucongleo@gmail.com>
2024-12-14 16:04:49 +0700

    update task filter

--- src/init.sql
+++ src/init.sql
@@ -14,6 +14,8 @@ CREATE TABLE "task"(
     status varchar(32) default 'TODO', -- 'IN_PROGRESS', 'DONE'
     priority varchar(32) default 'LOW', -- 'MEDIUM', 'HIGH'
     estimate_time int8 default 3600, -- seconds
+    created_date int8 DEFAULT (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT,
+    updated_date int8 DEFAULT (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT,
     is_deleted boolean default false
 );
 
@@ -23,3 +25,11 @@ CREATE TABLE "todo"(
     start_date int8 not null,
     end_date int8 not null
 );
+
+-- Inserting 3 example tasks
+INSERT INTO task (id_user, name, description, status, priority, estimate_time, created_date, updated_date, is_deleted)
+VALUES
+(1, 'Task 1', 'Description for task 1', 'IN_PROGRESS', 'HIGH', 7200, (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT, (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT, false),
+(2, 'Task 2', 'Description for task 2', 'DONE', 'LOW', 3600, (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT, (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT, false),
+(3, 'Task 3', 'Description for task 3', 'TODO', 'MEDIUM', 5400, (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT, (EXTRACT(EPOCH FROM now()) * 1000)::BIGINT, false);
+
